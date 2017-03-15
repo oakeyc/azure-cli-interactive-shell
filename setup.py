b1936d858f8348694dd35ae7f7fc6f366b1a0106
Courtney (CJ) Oka <oka.c@husky.neu.edu>
2017-03-14 17:06:10 -0700

    Windows Compatibility  (#23)
    * windows compat start
    
    * minor

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DEPENDENCIES = [
 
 setup(
     name='az-cli-shell',
-    version='0.1.1a23',
+    version='0.1.1a24',
     author='Microsoft Corporation',
     scripts=['dev_setup.py', 'az-cli'],
     packages=[
